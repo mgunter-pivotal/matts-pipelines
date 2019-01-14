bd8f3c6b98f621da5728c6494a48e6fccec5a28b
mgunter-pivotal <mgunter@pivotal.io>
2019-01-14 18:59:30 -0500

    --name

--- kubernetes/tasks/deploy-chart/task.sh
+++ kubernetes/tasks/deploy-chart/task.sh
@@ -7,4 +7,4 @@ cp kube-config/config ~/.kube/config
 helm init --client-only
 helm repo update
 cd chart-repo
-helm install $HELM_CHART --namespace $HELM_NAMESPACE --release $HELM_RELEASE
+helm install $HELM_CHART --namespace $HELM_NAMESPACE --name $HELM_RELEASE
